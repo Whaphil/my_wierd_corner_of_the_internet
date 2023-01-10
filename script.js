7e921595584a2355985cbf4f7c2915c220b20fd2
Philipp Sommer <p.sommer@smart-iot.solutions>
2023-01-10 20:44:17 +0100

    relative import

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-import {changeText} from "test.js"
+import {changeText} from "./test.js"
 const testButton = document.getElementById("test-button");
 testButton.addEventListener("click",(event)=>changeText(testButton))
